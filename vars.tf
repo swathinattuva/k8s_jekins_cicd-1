86fd03523394f6ff9d23c0b25d34214e215a5555
Swathinattuva <60080230+swathinattuva@users.noreply.github.com>
2020-04-22 16:41:32 -0400

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -7,11 +7,11 @@ variable "K8S_AMI_ID" {
 }
 
 variable "SUBNETID" {
-  default = "subnet-588a5131"
+  default = "subnet-9b2b1df3"
 }
 
 variable "VPCID" {
-  default = "vpc-2000df49"
+  default = "vpc-4e353c26"
 }
 
 variable "K8S_PRIV_KEY" {
